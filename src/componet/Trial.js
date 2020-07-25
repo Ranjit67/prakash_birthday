ba9c6378389ec72e1ccbd75cc9041f8e474f5806
Ranjit67 <sahooranjit519@gmail.com>
2020-07-25 08:30:49 +0530

    for frontend

--- src/componet/Trial.js
+++ src/componet/Trial.js
@@ -15,7 +15,7 @@ const userAgent = navigator.userAgent;
 const deviceer = deviceDetector.parse(userAgent);
 
 
-            fetch("https://dear-acc.herokuapp.com/",{
+            fetch(process.env.API_URL || "http://localhost:5000/",{
                 "method":"POST",
                 headers: {
                     'Accept': 'application/json',
